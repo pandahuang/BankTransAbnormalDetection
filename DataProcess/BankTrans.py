d66e422c38278cd3e6706a90ddb8ae9572423778
huangyixiang <huangchen_qwehc@gmail.com>
2016-03-27 15:28:18 +0800

    upload project

---
+++ DataProcess/BankTrans.py
@@ -0,0 +1,5 @@
+#-*-coding: utf-8-*-
+__author__ = 'panda_huang'
+
+class BankTrans(object):
+    pass
---
+++ Test/Statistic.py
@@ -0,0 +1,30 @@
+__author__ = 'panda_huang'
+
+DataPath = "G:\\projects\\FraudDetection\\Data\\1prtest.txt"
+BankTransData = []
+TransCardID = {}
+
+def ReadData(path = DataPath):
+    with open(path) as fopen:
+        for line in fopen.readlines():
+            dataline = line.strip().split('\t')
+            BankTransData.append(dataline)
+    pass
+
+def ShowDataStructure():
+    print(BankTransData)
+
+def CoutNum():
+    for line in BankTransData:
+        if TransCardID.has_key(line[0]):
+            TransCardID[line[0]] = TransCardID[line[0]] + 1
+        else:
+            TransCardID[line[0]] = 1
+    TransCardIDNum = TransCardID.__len__()
+    print("TransCardIDNum:", TransCardIDNum, TransCardID)
+
+if __name__ == "__main__":
+    ReadData()
+    ShowDataStructure()
+    CoutNum()
+    pass
