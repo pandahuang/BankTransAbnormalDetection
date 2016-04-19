f181d57d9e5931878e8e5b8b007933cfe72c486a
huangyixiang <huangyixiang.qwehc@gmail.com>
2016-04-20 00:51:15 +0800

    FindCycle and CountNodeDegree, But still have bugs.

--- GraphStructureVisualize/NetworkXTest.py
+++ GraphStructureVisualize/NetworkXTest.py
@@ -8,7 +8,7 @@ GraphFilePATH = "G:\\projects\\FraudDetection\\Data\\GraphData\\GraphFile.txt"
 Edges = []
 CentreNode = []
 LoadNum = 0
-LoadMax = 2
+LoadMax = 1
 with open(GraphFilePATH) as fopen:
     Lines = fopen.readlines()
     for line in Lines:
@@ -22,14 +22,26 @@ with open(GraphFilePATH) as fopen:
             break
 
 G = nx.DiGraph()
-# for node in CentreNode:
-#     G.add_node(node)
 for edge in Edges:
     SNode = edge.split('\t')[0]
     DNode = edge.split('\t')[1]
     G.add_edge(SNode, DNode)
 
-nx.draw_networkx(G, pos=nx.random_layout(G), arrows=False, node_size = 10, with_labels=False, width=0.2)
-plt.savefig("G:\\projects\\FraudDetection\\Data\\GraphData\\GraphPic.png")
-plt.show()
+GRAPHCYCLEPATH = "G:\\projects\\FraudDetection\\Data\\GraphData\\GraphCycle.txt"
+CycleList = list(nx.simple_cycles(G))
+with open(GRAPHCYCLEPATH, 'w') as fopen:
+    for line in CycleList:
+        fopen.write(line)
+        fopen.write('\n')
+
+GRAPHDEGREEPATH = "G:\\projects\\FraudDetection\\Data\\GraphData\\GraphDegree.txt"
+DegreeList = nx.degree(G)
+with open(GRAPHDEGREEPATH, 'w') as fopen:
+    for line in DegreeList:
+        fopen.write(line)
+        fopen.write('\n')
+
+# nx.draw_networkx(G, pos=nx.random_layout(G), arrows=False, node_size = 10, with_labels=False, width=0.2)
+# plt.savefig("G:\\projects\\FraudDetection\\Data\\GraphData\\GraphPic.png")
+# plt.show()
 
